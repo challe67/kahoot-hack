31718f9b1379d17f40764cc9b8ecf52ba6887901
challe67 <chalun02@edu.umea.se>
2025-11-18 09:36:31 +0100

    Update main.go

--- kahoot-crash/main.go
+++ kahoot-crash/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Fprintln(os.Stderr, "Usage: crash <game pin> <nickname>")
+		fmt.Fprintln(os.Stderr, "Usage: crash <9888751> <Enea & Alicia>")
 		os.Exit(1)
 	}
 	gamePin := os.Args[1]
